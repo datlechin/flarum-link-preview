878bcf121db694d58f0b29cdb6d6055b21c74daf
Ngo Quoc Dat <datlechin@gmail.com>
2022-08-29 23:25:31 +0700

    fix: update flarum workflows path

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,19 +1,21 @@
 import app from 'flarum/admin/app';
 
+const EXT_PREFIX = 'datlechin-link-preview';
+
 app.initializers.add('datlechin/flarum-link-preview', () => {
   app.extensionData
-    .for('datlechin-link-preview')
+    .for(EXT_PREFIX + '')
     .registerSetting({
-      setting: 'datlechin-link-preview.convert_media_urls',
-      label: app.translator.trans('datlechin-link-preview.admin.settings.convert_media_urls_label'),
-      help: app.translator.trans('datlechin-link-preview.admin.settings.convert_media_urls_help'),
+      setting: EXT_PREFIX + '.convert_media_urls',
+      label: app.translator.trans(EXT_PREFIX + '.admin.settings.convert_media_urls_label'),
+      help: app.translator.trans(EXT_PREFIX + '.admin.settings.convert_media_urls_help'),
       type: 'checkbox',
     })
     .registerSetting({
-      setting: 'datlechin-link-preview.blacklist',
-      label: app.translator.trans('datlechin-link-preview.admin.settings.blacklist_label'),
-      help: app.translator.trans('datlechin-link-preview.admin.settings.blacklist_help'),
-      placeholder: app.translator.trans('datlechin-link-preview.admin.settings.blacklist_placeholder'),
+      setting: EXT_PREFIX + '.blacklist',
+      label: app.translator.trans(EXT_PREFIX + '.admin.settings.blacklist_label'),
+      help: app.translator.trans(EXT_PREFIX + '.admin.settings.blacklist_help'),
+      placeholder: app.translator.trans(EXT_PREFIX + '.admin.settings.blacklist_placeholder'),
       type: 'textarea',
     });
 });
