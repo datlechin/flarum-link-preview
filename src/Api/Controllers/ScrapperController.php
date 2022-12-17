9c298f5a78b91c60b9caa7f7762c923d673a8267
Ngô Quốc Đạt <datlechin@gmail.com>
2022-12-18 00:40:32 +0700

    chore: update code

--- src/Api/Controllers/ScrapperController.php
+++ src/Api/Controllers/ScrapperController.php
@@ -8,17 +8,14 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface;
 use Spekulatius\PHPScraper\PHPScraper;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Throwable;
 
 class ScrapperController implements RequestHandlerInterface
 {
-    /**
-     * @var Spekulatius\PHPScraper\PHPScraper
-     */
+    /** @var PHPScraper $web */
     protected $web;
 
-    /**
-     * @var TranslatorInterface
-     */
+    /** @var TranslatorInterface $translator */
     protected $translator;
 
     public function __construct(PHPScraper $web, TranslatorInterface $translator)
@@ -33,10 +30,10 @@ class ScrapperController implements RequestHandlerInterface
      */
     public function handle(Request $request): Response
     {
-        $url = isset($request->getQueryParams()['url']) ? $request->getQueryParams()['url'] : '';
+        $url = $request->getQueryParams()['url'] ?? '';
         $domain = parse_url($url, PHP_URL_HOST);
 
-        if (filter_var($url, FILTER_VALIDATE_URL) === false || gethostbyname($domain) === $domain) {
+        if (! filter_var($url, FILTER_VALIDATE_URL) || gethostbyname($domain) === $domain) {
             return new JsonResponse([
                 'error' => $this->translator->trans('datlechin-link-preview.forum.site_cannot_be_reached')
             ]);
@@ -47,11 +44,11 @@ class ScrapperController implements RequestHandlerInterface
 
             return new JsonResponse([
                 'site_name' => $this->web->openGraph['og:site_name'] ?? $this->web->twitterCard['twitter:site'] ?? null,
-                'title' => $this->web->title ?? $this->web->openGraph['og:title']  ?? $this->web->twitterCard['twitter:title'] ?? null,
+                'title' => $this->web->title ?? $this->web->openGraph['og:title'] ?? $this->web->twitterCard['twitter:title'] ?? null,
                 'description' => $this->web->description ?? $this->web->openGraph['og:description'] ?? $this->web->twitterCard['twitter:description'] ?? null,
                 'image' => $this->web->image ?? $this->web->openGraph['og:image'] ?? $this->web->twitterCard['twitter:image'] ?? null,
             ]);
-        } catch (\Throwable $th) {
+        } catch (Throwable $th) {
             return new JsonResponse([
                 'error' => $th->getMessage(),
             ]);
