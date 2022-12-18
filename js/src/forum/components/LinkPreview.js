f5ab653807d41ac6da05d77dbd0a49adb3da3bbd
Ngo Quoc Dat <datlechin@gmail.com>
2022-12-18 21:31:28 +0700

    fix: prevent link preview text link

--- js/src/forum/components/LinkPreview.js
+++ js/src/forum/components/LinkPreview.js
@@ -1,70 +1,68 @@
-import Component from 'flarum/common/Component'
+import Component from 'flarum/common/Component';
 
 export default class LinkPreview extends Component {
   oninit(vnode) {
     super.oninit(vnode);
-    this.loading = true
-    this.link = vnode.attrs.link
-    this.data = null
+    this.loading = true;
+    this.link = vnode.attrs.link;
+    this.data = null;
 
-    this.fetchData()
+    this.fetchData();
   }
   view() {
     return (
       <div className="LinkPreview">
         <div className="LinkPreview-image">
-          {
-            this.loading
-              ? <i className="fa fa-spinner fa-spin" />
-              : <img src={(this.data?.image ?? 'https://www.google.com/s2/favicons?sz=64&domain_url=' + this.getDomain())} />
-          }
+          {this.loading ? (
+            <i className="fa fa-spinner fa-spin" />
+          ) : (
+            <img src={this.data?.image ?? 'https://www.google.com/s2/favicons?sz=64&domain_url=' + this.getDomain()} data-link-preview />
+          )}
         </div>
         <div className="LinkPreview-main">
           <div className="LinkPreview-title">
             <a href={this.link} target="_blank">
-              {this.loading ? this.getDomain() : (this.data?.title ?? this.data.error)}
+              {this.loading ? this.getDomain() : this.data?.title ?? this.data.error}
             </a>
           </div>
-          <div className="LinkPreview-description">
-            {this.loading ? '' : (this.data?.description ?? '')}
-          </div>
+          <div className="LinkPreview-description">{this.loading ? '' : this.data?.description ?? ''}</div>
           <div className="LinkPreview-domain">
-            <img src={'https://www.google.com/s2/favicons?sz=64&domain_url=' + this.getDomain()} />
+            <img src={'https://www.google.com/s2/favicons?sz=64&domain_url=' + this.getDomain()} data-link-preview />
             <a href={this.getHref()} target="_blank">
-              {this.loading ? this.getDomain() : (this.data?.site_name ?? this.getDomain())}
+              {this.loading ? this.getDomain() : this.data?.site_name ?? this.getDomain()}
             </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   oncreate(vnode) {
-    this.link.parentNode.insertBefore(vnode.dom, this.link)
+    this.link.parentNode.insertBefore(vnode.dom, this.link);
   }
 
   getHref() {
-    return this.link.href
+    return this.link.href;
   }
 
   getDomain() {
-    return this.getHref().split('/')[2]
+    return this.getHref().split('/')[2];
   }
 
   fetchData() {
     app
       .request({
-        url: app.forum.attribute('apiUrl') + '/datlechin-link-preview?url=' + encodeURIComponent(this.getHref()) ,
-        method: 'GET' ,
+        url: app.forum.attribute('apiUrl') + '/datlechin-link-preview?url=' + encodeURIComponent(this.getHref()),
+        method: 'GET',
       })
       .then((data) => {
-        this.setData(data)
-        this.loading = false
+        this.setData(data);
+        this.loading = false;
       });
   }
 
   setData(data) {
-    this.data = data
-    m.redraw()
+    this.data = data;
+    m.redraw();
   }
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,29 +3,29 @@ import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import LinkPreview from './components/LinkPreview';
 
-app.initializers.add('datlechin/flarum-link-preview' , () => {
-  extend(CommentPost.prototype , 'oncreate' , function () {
+app.initializers.add('datlechin/flarum-link-preview', () => {
+  extend(CommentPost.prototype, 'oncreate', function () {
     const blacklist = app.forum.attribute('datlechin-link-preview.blacklist');
     const blacklistArray = blacklist
       ? blacklist.split(',').map(function (item) {
-        return item.trim();
-      })
+          return item.trim();
+        })
       : [];
 
     const links = this.element.querySelectorAll('.Post-body a[rel]');
 
-    console.log(blacklistArray);
     links.forEach((link) => {
-      const href = link.href
+      const href = link.href;
       const domain = href.split('/')[2].split('.').slice(-2).join('.');
-      if (link.classList.contains('PostMention') || link.classList.contains('UserMention')) return
-      if (blacklistArray.includes(domain)) return
+
+      if (link.classList.contains('PostMention') || link.classList.contains('UserMention')) return;
+      if (blacklistArray.includes(domain) || href !== link.textContent) return;
       if (app.forum.attribute('datlechin-link-preview.convertMediaURLs') && href.match(/\.(jpe?g|png|gif|svg|webp|mp3|mp4|m4a|wav)$/)) return;
 
-      m.mount(link , {
+      m.mount(link, {
         view: function () {
-          return m(LinkPreview , { link });
-        }
+          return m(LinkPreview, { link });
+        },
       });
     });
   });
